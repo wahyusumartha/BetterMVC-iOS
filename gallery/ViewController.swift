3d63825ec2a88bd423e77ee0d4152a16cf14f083
wahyu sumartha <wahyusumartha@gmail.com>
2016-08-06 22:19:24 +0700

    Initial Commit

---
+++ gallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gallery
+//
+//  Created by Wahyu Sumartha  on 06/08/2016.
+//  Copyright © 2016 Wahyu Sumartha . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
